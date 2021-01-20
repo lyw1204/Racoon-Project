6a087ff6595e8a939cfb96631f01c8d076fd57d0
David Liu <lyw120498@gmail.com>
2021-01-19 21:24:10 -0500

    Updated scanner to take i2c commands

--- scanner_optimizing/scanner_optimizing.ino
+++ scanner_optimizing/scanner_optimizing.ino
@@ -1,6 +1,4 @@
 #include <Wire.h>
-
-
 #include <Stepper.h>
 #include <Servo.h>
 #include <NewPing.h>
@@ -13,8 +11,8 @@
 #define STEPPER_P4 7
 #define SERVO_P1 3
 #define LIM_SW1 2
-#define SONIC_TRIG 8
-#define SONIC_ECHO 9
+#define SONIC_TRIG 9
+#define SONIC_ECHO 8
 
 #define POWER_PIN 10 
 
@@ -35,8 +33,13 @@
 //Matrix Parameters
 #define X_RES 27 // size of depth map is 3888 bits
 #define Y_RES 9
-#define SAMPLES 5
+#define SAMPLES 3
 #define MAXDIST 200
+
+//I2C Configuration
+#define SLAVE_ADDR 8
+#define BUFFER_SIZE 16
+
 
 
 //Global variable for depth map accesses
@@ -102,13 +105,6 @@ void medianFilter(short matrix[Y_RES][X_RES]) {//Applies median filter on matrix
 
     }
   }
-  /*
-    for(int i = 0; i<Y_RES; i++){//Zero fill the matrix
-    for(int j = 0; j<X_RES; j++){
-      matrix[i][j] = 0;
-      }
-    }
-  */
   //Edge pixels will remain unchanged
   for (int i = 1; i < Y_RES - 1; i++) {
     for (int j = 1; j < X_RES - 1; j++) { //Change all pixels but the outer edge
@@ -125,7 +121,6 @@ bool judgeMatrix(short matrix[Y_RES][X_RES]){//Returns true if human, else false
   short colAccum[Y_RES-2];
   char topRow = 0;
   char botRow = 0;
-  
   
   for(short i = 1; i<Y_RES-1; i++){
     colAccum[i-1] = 0;//Initializes colAccum array elements
@@ -158,17 +153,13 @@ bool judgeMatrix(short matrix[Y_RES][X_RES]){//Returns true if human, else false
       }
     } 
 
-   if(abs(botRow-topRow) > 3){
+   if(abs(botRow-topRow) >= 3){
     return true;
     }
    else{
     return false; 
     }
   }
-
-//I2C communications handling code
-
-
 
 
 
@@ -198,7 +189,32 @@ class Scanner {
         return result;
       }
     }
+    
+    
+    void goToPos(int x, int y) {
 
+      //soft guard for invalid positions
+      x = x % 360;
+
+      stepperX->step((x - xPos) / (360.0 / stepsPerRotation));
+      servoY.write(y);
+
+      xPos = x;
+      yPos = y;
+    }
+    void powerOn() {
+      //Connect power to stepper and servos
+      scanner_en = true;
+      servoY.attach(servoPin);
+      digitalWrite(powerPin, HIGH);
+    }
+
+    void powerOff() {
+      //Disconnect powerto stepper and servos
+      scanner_en = false;
+      servoY.detach();
+      digitalWrite(powerPin,LOW);
+    }
 
   public:
     Scanner(int step1, int step2, int step3, int step4, int stepRot, byte stepSpd, byte serv, byte limSw, byte sonicTrig, byte sonicEcho, byte power) {
@@ -214,8 +230,6 @@ class Scanner {
       sonicEchoPin = sonicEcho;
 
       powerPin = power;
-
-
 
       scanner_en = false;
       scanner_homed = false;
@@ -243,21 +257,6 @@ class Scanner {
 
     }
 
-    void powerOn() {
-      //Connect power to stepper and servos
-      scanner_en = true;
-      servoY.attach(servoPin);
-      digitalWrite(powerPin, HIGH);
-    }
-
-    void powerOff() {
-      //Disconnect powerto stepper and servos
-      this->scanner_en = false;
-      servoY.detach();
-      digitalWrite(powerPin,LOW);
-    }
-
-
     void goHome() {
       powerOn();
       servoY.write(Y_OFFSET);
@@ -273,75 +272,140 @@ class Scanner {
       delay(300);
     }
 
-    void goToPos(int x, int y) {
-      //Connect power to servo and stepper
-      this->scanner_en = true;
-
-      //soft guard for invalid positions
-      x = x % 360;
-
-
-      if (!this->scanner_homed) {
-        //this->goHome();
-      }
-      stepperX->step((x - xPos) / (360.0 / stepsPerRotation));
-      servoY.write(y);
-
-      xPos = x;
-      yPos = y;
-    }
-
-
     void scanFaster (short matrix [Y_RES][X_RES]) {
+      powerOn();
+      goHome();
       for (int j = 0; j < X_RES; j++) {
         for (int i = 0; i < Y_RES; i++) {
           if (j % 2 == 0) {
             goToPos(j * 10, Y_OFFSET - i * 10);
             matrix[i][j] = avgSensor(SAMPLES);
-
           }
           else {
             goToPos(j * 10, Y_OFFSET - (Y_RES - 1 - i) * 10);
             matrix[Y_RES - 1 - i][j] = avgSensor(SAMPLES);
           }
-
-
-
         }
 
-
-
-
       }
-
-
       //Return scanner back to its resting position facing back
       goToPos(-45, 0);
       scanner_homed = false;
-
+      powerOff();  
+      medianFilter(matrix);//Make it look nice and pretty
     }
-
 };
-
 
 //Initialize new scanner
 Scanner myScanner(STEPPER_P1, STEPPER_P2, STEPPER_P3, STEPPER_P4, STEPS_PER_ROT, STEPPER_SPD, SERVO_P1, LIM_SW1, SONIC_TRIG, SONIC_ECHO, POWER_PIN);
 
+
+//I2C communications
+class Buffer{//implements cyclic buffer for tx and rx
+  private:
+    int _size;
+    byte * _stack;
+    byte _top = 0;
+    byte _bottom = 0;
+    
+  public:
+    Buffer(int SIZE){
+      _size = SIZE;
+      _stack = new byte [_size];
+      }
+
+    void push(byte val){
+      _stack [_top] = val;
+      _top ++;
+      _top = _top % _size;     
+      }
+
+    byte pop(){
+      byte retVal = _stack[_bottom];
+      _bottom++;
+      _bottom = _bottom % _size;
+      return retVal;
+      }
+
+    bool isEmpty(){
+      return(_bottom == _top); 
+      }
+  };
+
+void slaveBegin(){
+  Wire.begin(SLAVE_ADDR);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
+  }
+
+
+Buffer txBuffer(BUFFER_SIZE);
+Buffer rxBuffer(BUFFER_SIZE);
+
+void requestEvent(){
+  //Pop txBuffer and write onto I2C
+  Wire.write(txBuffer.pop());
+  }
+
+void receiveEvent(){
+  rxBuffer.push(Wire.read());
+  }
+
+void slaveExecute(){
+  byte cmd = rxBuffer.pop();//pop a commannd from rxBuffer
+  
+  switch (cmd){//Interprets different commands and do different things
+    case 0: //scanner home
+      myScanner.goHome();
+      txBuffer.push(0);
+      break;
+
+    case 1: //scan baseline, return 0
+      myScanner.scanFaster(depthBaseline);
+      txBuffer.push(0);
+      break;
+
+    case 2: //scan now, return judgement 1/0
+      myScanner.scanFaster(depthNow);
+      diffMatrix(depthBaseline, depthNow);//Subtract baseline from depthNow
+      txBuffer.push(judgeMatrix(depthNow));
+      break;
+
+    case 3: //random alarm, return 0
+      Serial.println("ALARM!");
+      txBuffer.push(0);
+      break;
+
+    case 4: //self, test, return 
+      Serial.println("SELF-TEST DUMMY PROGRAM");
+      txBuffer.push(0b00000011); //Fake test failure here, just for fun
+      
+      break;
+
+    case 5: //reserved
+      txBuffer.push(0);
+      break;
+
+    case 6: //reserved
+      txBuffer.push(0);
+      break;
+
+    default://Do nothing
+      txBuffer.push(0b11111111);//0b11111111 indicates trransmission failure to 8266, host should retransmit
+      break;
+
+    }
+  }
+
+
 void setup() {
-  myScanner.goHome();
   Serial.begin(9600);
-  myScanner.scanFaster(depthBaseline);//Populates a baseline image
-  delay(10000);
-  myScanner.scanFaster(depthNow);
-
-  diffMatrix(depthBaseline, depthNow);//Subtract baseline from depthNow
-  medianFilter(depthNow);//Make it look nice and pretty
-  printMatrix(depthNow);//printout
-  Serial.print(judgeMatrix(depthNow));
-  digitalWrite(POWER_PIN, LOW);
-
+  rxBuffer.push(1);
+  rxBuffer.push(2);
 }
 
 void loop() {
-
+  while(rxBuffer.isEmpty()==0){
+    slaveExecute();    
+    }
 }
